0ed8a661d05361329b1a4cba1c83f7b62b5de00c
Heikki Myllykoski <heikki.j.myllykoski@gmail.com>
2021-11-20 15:13:05 +0200

    teemun väriplotti

--- main.py
+++ main.py
@@ -7,10 +7,34 @@ app = Flask(__name__)
 def index():
     start_coords = (60.1666, 24.9436)
     folium_map = folium.Map(location=start_coords, zoom_start=14)
-    folium.GeoJson("kattavuusdata/4G/4G_tahtiluokka_1.json", name="geojson").add_to(folium_map)
-    folium.GeoJson("kattavuusdata/4G/4G_tahtiluokka_2.json", name="geojson").add_to(folium_map)
-    folium.GeoJson("kattavuusdata/4G/4G_tahtiluokka_3.json", name="geojson").add_to(folium_map)
     
+    tahtiluokka1 = "kattavuusdata/4G/4G_tahtiluokka_1.json"
+    tahtiluokka2 = "kattavuusdata/4G/4G_tahtiluokka_2.json"
+    tahtiluokka3 = "kattavuusdata/4G/4G_tahtiluokka_3.json"
+    folium.Choropleth(
+        geo_data=tahtiluokka1, 
+        name="geojson",
+        fill_color="red",
+        fill_opacity=0.7,
+        line_color="red",
+        line_opacity=0.1
+    ).add_to(folium_map)
+    folium.Choropleth(
+        geo_data=tahtiluokka2, 
+        name="geojson",
+        fill_color="yellow",
+        line_color="yellow",
+        fill_opacity=0.7,
+        line_opacity=0.1
+    ).add_to(folium_map)
+    folium.Choropleth(
+        geo_data=tahtiluokka3, 
+        name="geojson",
+        fill_color="green",
+        line_color="green",
+        fill_opacity=0.7,
+        line_opacity=0.1
+    ).add_to(folium_map)
     
     return folium_map._repr_html_()
 
