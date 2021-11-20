30259886cf3ce347c46c76101b8b449b3d9953e9
Heikki Myllykoski <heikki.j.myllykoski@gmail.com>
2021-11-20 18:43:17 +0200

    satasen laina

--- data_modification.py
+++ data_modification.py
@@ -1,5 +1,7 @@
 import csv
+from os import name
 import pandas as pd
+import geojson as gj
 
 cz = open("cell_zone.csv")
 csvreader = csv.reader(cz)
@@ -19,3 +21,24 @@ data_final = data_final[((pd.to_numeric(data_final["longitude"]) > 23.97) & (pd.
 
 data_final.to_csv("data_final.csv", index=False)
 
+# Iteroidaan kaikki json alueet ja sit poistetaan ne palikat joissa on jokin piste alueen ulkopuolella
+
+tahtiluokka1 = "kattavuusdata/4G/4G_tahtiluokka_1.json"
+tahtiluokka2 = "kattavuusdata/4G/4G_tahtiluokka_2.json"
+tahtiluokka3 = "kattavuusdata/4G/4G_tahtiluokka_3.json"
+
+files_4g = [tahtiluokka1] #, tahtiluokka2, tahtiluokka3]
+
+for file in files_4g:
+    with open(file) as f:
+        stuff = gj.load(f)
+    
+    for feature in stuff["features"]:
+        is_not_a_ok_polygon = False
+        for point in feature["geometry"]["coordinates"][0]:
+            if (point[0] < 23.97) or (point[0] > 25.47) or (point[1] > 60.40):
+                is_ok_polygon = True
+                break
+        if is_not_a_ok_polygon:
+            #Heitetään vittuun
+            
